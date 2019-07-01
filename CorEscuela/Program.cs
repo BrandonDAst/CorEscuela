af76b350f50d526f4e1a3e547c10949f35d628a5
brandon <brandon.diaz.as@gmail.com>
2019-07-01 13:04:34 -0500

    Se agregó la base para crear el objto Escuela con atributos y metodos

--- CorEscuela/Program.cs
+++ CorEscuela/Program.cs
@@ -2,12 +2,31 @@
 
 namespace CorEscuela
 {
+    public class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int anoFundacion;
+        public void Timbrar()
+        {
+            //Todo
+            Console.Beep(1000, 5000);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iniciando CorEscuela");
+            Escuela platzi = new Escuela
+            {
+                nombre = "Platzi Academy",
+                direccion = "Internet",
+                anoFundacion = 2008
+            };
+            Console.WriteLine($"La escuela se llama {platzi.nombre}, y se encuentra en {platzi.direccion}, y se fundó en el año {platzi.anoFundacion}.");
+            Console.WriteLine($"Hagamosla timbrar...");
+            platzi.Timbrar();
         }
     }
 }
