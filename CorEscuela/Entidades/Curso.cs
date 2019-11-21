813294777c20dfc02fcf34932aece2451e13160e
brandon <brandon.diaz.as@gmail.com>
2019-11-21 15:58:43 -0600

    Clase curso añadida

---
+++ CorEscuela/Entidades/Curso.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorEscuela.Entidades
+{
+    public class Curso
+    {
+        private string nombre;
+        public string Nombre
+        {
+            get { return nombre.ToUpper(); }
+            set { nombre = value; }
+        }
+        private int grado;
+
+        public int Grado
+        {
+            get { return grado; }
+            set { grado = value; }
+        }
+
+
+        public Curso(string nombre, int grado) => (Nombre, Grado) = (nombre, grado);
+
+        public override string ToString()
+        {
+            return $"Curso: {nombre}. Grado: {grado}";
+        }
+    }
+}
